5e81590e09575ef9a026fac5d1e4b9b39973b871
Behruzaxi <ebehruz846@gmail.com>
2023-05-17 00:42:34 +0500

    fsss

---
+++ index.js
@@ -0,0 +1,22 @@
+
+function calc(){
+    let n1 = parseFloat(document.getElementById('n1').value);
+    let n2 = parseFloat(document.getElementById('n2').value);
+    let oper = parseFloat(document.getElementById('operators').value);
+
+     if(oper === '+'){
+        document.getElementById('result').value = n1 + n2;
+     }
+     if(oper === '-'){
+        document.getElementById('result').value = n1 - n2;
+     }
+     if(oper === '/'){
+        document.getElementById('result').value = n1 / n2;
+     }
+     if(oper === '*'){
+        document.getElementById('result').value = n1 * n2;
+     }
+
+}
+
+console.log(calc());
