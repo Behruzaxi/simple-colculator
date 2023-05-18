28eaa58cd407061774bf86fe702488177658c998
Behruzaxi <ebehruz846@gmail.com>
2023-05-18 15:10:05 +0500

    fds

--- index.js
+++ index.js
@@ -1,22 +1,59 @@
+const n1 = document.querySelector('.n1');
+const n2 = document.querySelector('.n2');
 
-function calc(){
-    let n1 = parseFloat(document.getElementById('n1').value);
-    let n2 = parseFloat(document.getElementById('n2').value);
-    let oper = parseFloat(document.getElementById('operators').value);
+const submit = document.querySelector('#submit');
+const asign =  document.querySelector('#asign');
+const result = document.querySelector('#result');
 
-     if(oper === '+'){
-        document.getElementById('result').value = n1 + n2;
-     }
-     if(oper === '-'){
-        document.getElementById('result').value = n1 - n2;
-     }
-     if(oper === '/'){
-        document.getElementById('result').value = n1 / n2;
-     }
-     if(oper === '*'){
-        document.getElementById('result').value = n1 * n2;
-     }
+submit.addEventListener("click", () => {
+    let finalResult;
 
-}
+    const firstNumber = n1.value
+    const secondNumber = n2.value
+    const operation = asign.value
 
-console.log(calc());
+    if(operation == "+") {
+        finalResult = Number(firstNumber) + Number(secondNumber)
+    }
+    else if(operation == "-"){
+        finalResult = Number(firstNumber) - Number(secondNumber)
+    }
+    else if(operation == "*"){
+        finalResult = Number(firstNumber) * Number(secondNumber)
+    }
+    else if(operation == "/"){
+        finalResult = Number(firstNumber) / Number(secondNumber)
+    }
+
+
+
+    result.innerHTML = finalResult
+})
+
+
+
+
+
+
+
+// function calc(){
+//     let n1 = parseFloat(document.getElementById('n1').value);
+//     let n2 = parseFloat(document.getElementById('n2').value);
+//     let oper = parseFloat(document.getElementById('operators').value);
+
+//      if(oper === '+'){
+//         document.getElementById('result').value = n1 + n2;
+//      }
+//     else if(oper === '-'){
+//         document.getElementById('result').value = n1 - n2;
+//      }
+//     else if(oper === '/'){
+//         document.getElementById('result').value = n1 / n2;
+//      }
+//      else if(oper === '*'){
+//         document.getElementById('result').value = n1 * n2;
+//      }
+
+// }
+
+// console.log(calc());
